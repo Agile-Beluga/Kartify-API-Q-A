f4cae08cf345c48df9214c94224a933e1bea4358
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-01-10 16:29:56 -0500

    Change server port

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const router = require('./routes/routes.js');
 const app = express();
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use('/', router);
 
-const port = 2020;
+const port = 3000;
 app.listen(port, () => console.log(`Web server listening on port ${port}`));
